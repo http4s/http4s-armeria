ab78eea86e161378964780ca31f4c0500b91900e
danicheg <esik.danny@gmail.com>
2024-12-08 20:54:22 +0300

    Add ArmeriaServerBuilder#defaultServiceErrorHandler

--- server/src/main/scala/org/http4s/armeria/server/ArmeriaServerBuilder.scala
+++ server/src/main/scala/org/http4s/armeria/server/ArmeriaServerBuilder.scala
@@ -16,12 +16,25 @@
 
 package org.http4s.armeria.server
 
+import java.io.{File, InputStream}
+import java.net.InetSocketAddress
+import java.security.PrivateKey
+import java.security.cert.X509Certificate
+import java.util.function.{Function => JFunction}
+import javax.net.ssl.KeyManagerFactory
+
+import cats.Monad
 import cats.effect.{Async, Resource}
-import cats.syntax.applicative._
-import cats.syntax.flatMap._
-import cats.syntax.functor._
+import cats.effect.std.Dispatcher
+import cats.syntax.all._
 import com.linecorp.armeria.common.util.Version
-import com.linecorp.armeria.common.{HttpRequest, HttpResponse, SessionProtocol, TlsKeyPair}
+import com.linecorp.armeria.common.{
+  ContentTooLargeException,
+  HttpRequest,
+  HttpResponse,
+  SessionProtocol,
+  TlsKeyPair
+}
 import com.linecorp.armeria.server.{
   HttpService,
   HttpServiceWithRoutes,
@@ -33,18 +46,10 @@ import com.linecorp.armeria.server.{
 import io.micrometer.core.instrument.MeterRegistry
 import io.netty.channel.ChannelOption
 import io.netty.handler.ssl.SslContextBuilder
-
-import java.io.{File, InputStream}
-import java.net.InetSocketAddress
-import java.security.PrivateKey
-import java.security.cert.X509Certificate
-import java.util.function.{Function => JFunction}
-import cats.effect.std.Dispatcher
 import com.comcast.ip4s
-
-import javax.net.ssl.KeyManagerFactory
 import org.http4s.armeria.server.ArmeriaServerBuilder.AddServices
-import org.http4s.{BuildInfo, HttpApp, HttpRoutes}
+import org.http4s.headers.{Connection, `Content-Length`}
+import org.http4s.{BuildInfo, Headers, HttpApp, HttpRoutes, Request, Response, Status}
 import org.http4s.server.{
   DefaultServiceErrorHandler,
   Server,
@@ -370,7 +375,7 @@ object ArmeriaServerBuilder {
     * for some errors propagated from the Armeria side.
     */
   def defaultServiceErrorHandler[F[_]](implicit
-      F: Monad[F]): Request[F] => PartialFunction[Throwable, F[Response[F]]] = {
+      F: Monad[F], LF: LoggerFactory[F]): Request[F] => PartialFunction[Throwable, F[Response[F]]] = {
     val contentLengthErrorHandler: Request[F] => PartialFunction[Throwable, F[Response[F]]] =
       req => { case _: ContentTooLargeException =>
         Response[F](
@@ -383,6 +388,6 @@ object ArmeriaServerBuilder {
         ).pure[F]
       }
 
-    req => contentLengthErrorHandler(req).orElse(DefaultServiceErrorHandler(F)(req))
+    req => contentLengthErrorHandler(req).orElse(DefaultServiceErrorHandler(LF, F)(req))
   }
 }
